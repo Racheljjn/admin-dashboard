aee6a3f3f38a9615f86a804d49b1b7fb42b34630
Racheljjn <jian1160@mylaurier.ca>
2022-07-04 00:20:00 -0400

    add line chart and notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { chatData } from '../data/dummy';
 
 const Notification = () => {
   return (
-    <div>Notification</div>
+    <div className="shadow-md nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
+      <h1 className='font-semibold '>Notification</h1>
+
+    </div>
   )
 }
 
